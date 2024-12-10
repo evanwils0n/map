ef1c44576acae1f7dca73472cb4d901b3f2872b8
evanwils0n <evanjwilson@hotmail.com>
2024-12-09 22:21:03 -0500

    Update

---
+++ src/map.js
@@ -0,0 +1,7 @@
+"use strict";
+// Initialize the map and set its view
+const map = L.map('map').setView([51.505, -0.09], 13);
+// Add a tile layer (e.g., OpenStreetMap)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
