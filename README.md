36afbf1a3971b7dc4b70d8dee1b71ba5b4a61668
evanwils0n <evanjwilson@hotmail.com>
2025-02-06 21:29:26 -0500

    Slim Down

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
 ### I am working on this because I have nothing better to do.
 
 #### Started 12/9/2024
+
+Make sure Vite is installed.
+`npm install --save-dev vite`
+
+Start dev Vite environment server by running:
+`npm run dev`
+
+*Keep in mind, that `npx tsc` needs to be run in order to compile any new editions.
+
--- src/map.js
+++ src/map.js
@@ -1,7 +1,34 @@
-"use strict";
+import L from 'leaflet';
+import 'leaflet-draw';
 // Initialize the map and set its view
 const map = L.map('map').setView([51.505, -0.09], 13);
-// Add a tile layer (e.g., OpenStreetMap)
+// Add a tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+// Initialize the FeatureGroup for drawn layers
+const drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+// Add the Leaflet.draw control
+const drawControl = new L.Control.Draw({
+    edit: {
+        featureGroup: drawnItems
+    },
+    draw: {
+        polygon: {}, // Default PolygonOptions
+        polyline: {}, // Default PolylineOptions
+        rectangle: {}, // Default RectangleOptions
+        circle: {}, // Default CircleOptions
+        marker: {} // Default MarkerOptions
+    }
+});
+map.addControl(drawControl);
+// Helper function for registering draw events
+function onDrawCreated(map, eventType, callback) {
+    map.on(eventType, callback);
+}
+// Register the 'draw:created' event to add shapes to the map
+onDrawCreated(map, L.Draw.Event.CREATED, (event) => {
+    const layer = event.layer; // Access the drawn layer
+    drawnItems.addLayer(layer); // Add it to the FeatureGroup
+});
